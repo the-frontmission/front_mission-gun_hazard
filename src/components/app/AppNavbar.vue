920327b0e27d3a768eac4ab6cc8873a6f1153b46
GimYoungPhil <youngphil.gim@gmail.com>
2018-11-08 02:40:37 +0900

    fixed AppNavbar

--- src/components/app/AppNavbar.vue
+++ src/components/app/AppNavbar.vue
@@ -1,20 +1,41 @@
 <template>
   <nav class="navbar navbar-expand navbar-dark flex-column flex-md-row bg-dark">
     <a class="navbar-brand" href="#">FRONT MISSION: Gun Hazard</a>
-    <div class="table-responsive">
+    <div class="w-100">
       <ul class="navbar-nav flex-row justify-content-around justify-content-md-center">
-        <li class="nav-item active">
-          <router-link to="/" class="nav-link">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/about" class="nav-link">About</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/scripts" class="nav-link">Scripts</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/videos" class="nav-link">Video</router-link>
-        </li>
+        <router-link
+          tag="li"
+          class="nav-item"
+          active-class="active"
+          to="/"
+          exact
+        >
+          <a class="nav-link">Home</a>
+        </router-link>
+        <router-link
+          tag="li"
+          class="nav-item"
+          active-class="active"
+          to="/about"
+        >
+          <a class="nav-link">About</a>
+        </router-link>
+        <router-link
+          tag="li"
+          class="nav-item"
+          active-class="active"
+          to="/scripts"
+        >
+          <a class="nav-link">Scripts</a>
+        </router-link>
+        <router-link
+          tag="li"
+          class="nav-item"
+          active-class="active"
+          to="/videos"
+        >
+          <a class="nav-link">Videos</a>
+        </router-link>
       </ul>
     </div>
   </nav>
