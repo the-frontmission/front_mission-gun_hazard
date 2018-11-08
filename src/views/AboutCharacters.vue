27896e220071b819969794b3d302f2306ee25636
GimYoungPhil <youngphil.gim@gmail.com>
2018-11-09 02:10:34 +0900

    fixed AboutCharacters

--- src/views/AboutCharacters.vue
+++ src/views/AboutCharacters.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
     <h1>Characters</h1>
+    <CharactersList :characters="characters"/>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+import CharactersList from '@/components/aboutCharacters/CharactersList.vue'
+
 export default {
   name: 'AboutCharacters',
+  components: {
+    CharactersList,
+  },
+  computed: {
+    ...mapState('characters', {
+      characters: 'list',
+    }),
+  },
+  methods: {
+    ...mapActions('characters', {
+      fetch: 'getCharacters',
+    }),
+  },
+  mounted() {
+    this.fetch()
+  },
 }
 </script>
 
