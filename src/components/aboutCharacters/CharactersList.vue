f97b4c64c408e35ccdf990cac468fa8aacf19f74
GimYoungPhil <youngphil.gim@gmail.com>
2018-11-09 02:10:43 +0900

    add CharactersList

---
+++ src/components/aboutCharacters/CharactersList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <template v-for="ch in characters">
+      <div :key="ch.uid">
+        {{ ch.nameJP }}
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharactersList',
+  props: {
+    characters: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
