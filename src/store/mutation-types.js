98ce0cca300814d86bfda9266fbd908b9037285b
GimYoungPhil <youngphil.gim@gmail.com>
2018-11-09 02:09:41 +0900

    add mutation-types

---
+++ src/store/mutation-types.js
@@ -0,0 +1,14 @@
+// game
+export const REQUEST_GAME = 'game/REQUEST_GAME'
+export const RECEIVE_GAME = 'game/RECEIVE_GAME'
+export const FAILURE_GAME = 'game/FAILURE_GAME'
+
+// characters
+export const REQUEST_CHARACTERS = 'characters/REQUEST_CHARACTERS'
+export const RECEIVE_CHARACTERS = 'characters/RECEIVE_CHARACTERS'
+export const FAILURE_CHARACTERS = 'characters/FAILURE_CHARACTERS'
+
+// items
+export const REQUEST_ITEMS = 'items/REQUEST_ITEMS'
+export const RECEIVE_ITEMS = 'items/RECEIVE_ITEMS'
+export const FAILURE_ITEMS = 'items/FAILURE_ITEMS'
