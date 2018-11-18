da91bf9ab173529d448342c531d7b644c042be07
GimYoungPhil <youngphil.gim@gmail.com>
2018-11-18 23:33:29 +0900

    add AppFooter

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,18 @@
   <div id="app">
     <AppNavbar/>
     <router-view/>
+    <AppFooter/>
   </div>
 </template>
 
 <script>
 import AppNavbar from '@/components/app/AppNavbar.vue'
+import AppFooter from '@/components/app/AppFooter.vue'
 
 export default {
   components: {
     AppNavbar,
+    AppFooter,
   },
 }
 </script>
-
-<style></style>
---
+++ src/components/app/AppFooter.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer class="bg-dark">
+    <div class="container-fluid p-3 p-md-5">
+      <div class="row">
+        <div class="col">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">About</a>
+              <ul>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Game</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Characters</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Items</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Scripts</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Videos</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col"></div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
