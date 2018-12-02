be37e35c8177c210e9d8f5b2c72f81079272ac21
GimYoungPhil <youngphil.gim@gmail.com>
2018-12-02 17:17:13 +0900

    fixed Characters

---
+++ src/views/CharactersView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <CharacterGrid
+      v-for="character in characters"
+      :key="character.uid"
+      :character="character"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+import CharactersGrid from '@/components/characters/CharactersGrid.vue'
+import CharacterGrid from '@/components/characters/CharacterGrid.vue'
+
+export default {
+  name: 'CharactersView',
+  components: {
+    CharactersGrid,
+    CharacterGrid,
+  },
+  computed: {
+    ...mapState('characters', {
+      characters: 'list',
+    }),
+  },
+  methods: {
+    ...mapActions('characters', {
+      fetch: 'getCharacters',
+    }),
+  },
+  mounted() {
+    this.fetch()
+  },
+}
+</script>
