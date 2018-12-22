89390fbb1dd6977e2ff0e50e1e0803815738a721
GimYoungPhil <youngphil.gim@gmail.com>
2018-12-22 18:06:16 +0900

    fixed ScriptRow

--- src/components/scripts/ScriptRow.vue
+++ src/components/scripts/ScriptRow.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <div class="row border my-5" v-for="(script, index) in scripts" :key="index">
-      <div class="col-lg-2" v-for="(chapter, index) in script.chapters" :key="index">
-        <button type="button" class="btn btn-primary">
-          {{ chapter.sequence }}
-        </button>
-        <p>{{ chapter.chapter }}</p>
+    <div
+      class="row no-gutters border text-center my-3 p-3"
+      v-for="(script, index) in scripts"
+      :key="index"
+    >
+      <div
+        class="col-lg-2 gh-chapter"
+        v-for="(chapter, index) in script.chapters"
+        :key="index"
+      >
+        <div class="d-flex flex-row flex-lg-column align-items-center">
+          <button type="button" class="btn btn-dark text-center p-0">
+            {{ chapter.sequence }}
+          </button>
+          <p class="ml-3 ml-lg-0 mb-0 mt-0 mt-lg-1">
+            {{ chapter.chapter }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -24,11 +36,20 @@ export default {
 </script>
 
 <style scoped>
-.btn-primary {
-  color: #fff;
-  background-color: #007bff;
-  border-color: #007bff;
-  height: 2.5rem;
-  width: 2.5rem;
+.btn-dark {
+  height: 3rem;
+  width: 3rem;
+  font-size: 1.5rem;
+}
+.gh-chapter+.gh-chapter {
+  margin-top: 1rem;
+}
+@media screen and (min-width: 992px) {
+  .gh-chapter+.gh-chapter {
+    margin-top: 0;
+  }
+}
+.col-lg-2:nth-child(n+7) {
+  margin-top: 1rem;
 }
 </style>
