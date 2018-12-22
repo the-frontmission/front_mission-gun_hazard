1742755b6e9427603f987d982c2aba185df00ba7
GimYoungPhil <youngphil.gim@gmail.com>
2018-12-23 00:44:23 +0900

    fixed ScriptsView

--- src/views/ScriptsView.vue
+++ src/views/ScriptsView.vue
@@ -1,32 +1,12 @@
 <template>
-  <div class="container">
-    <ScriptRow :scripts="scripts"/>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-import ScriptRow from '@/components/scripts/ScriptRow.vue'
 
 export default {
   name: 'ScriptsView',
-  components: {
-    ScriptRow,
-  },
-  computed: {
-    ...mapState('scripts', {
-      scripts: 'list',
-    }),
-  },
-  methods: {
-    ...mapActions('scripts', {
-      fetch: 'getScripts',
-    }),
-  },
-  mounted() {
-    this.fetch()
-  },
 }
 </script>
-
-<style scoped></style>
