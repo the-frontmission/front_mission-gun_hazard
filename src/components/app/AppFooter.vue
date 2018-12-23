9e2a490974339cf2bbde4a1091893ff3c8920cfb
GimYoungPhil <youngphil.gim@gmail.com>
2018-12-23 16:54:13 +0900

    fixed AppFooter

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -1,24 +1,25 @@
 <template>
-  <footer class="bg-dark">
+  <footer class="bg-dark text-white">
     <div class="container-fluid p-3 p-md-5">
-      <div class="row">
-          <ul class="nav">
-            <template v-for="(route, index) in routes">
-              <router-link
-                :key="index"
-                class="nav-item"
-                tag="li"
-                :to="{ path: route.path }"
-                active-class="active"
-                exact
-              >
-                <a class="nav-link text-capitalize">
-                  {{ route.label }}
-                </a>
-              </router-link>
-            </template>
-          </ul>
-      </div>
+        <ul class="nav">
+          <template v-for="(route, index) in routes">
+            <router-link
+              :key="index"
+              class="nav-item"
+              tag="li"
+              :to="{ path: route.path }"
+              active-class="active"
+              exact
+            >
+              <a class="nav-link text-capitalize">
+                {{ route.label }}
+              </a>
+            </router-link>
+          </template>
+        </ul>
+        <p>
+          Designed and built by <a href="https://github.com/GimYoungPhil" target="_blank">@phil</a>.
+        </p>
     </div>
   </footer>
 </template>
@@ -36,4 +37,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.nav-item:first-child .nav-link {
+  padding-left: 0;
+}
+</style>
