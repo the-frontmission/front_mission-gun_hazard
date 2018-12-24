711d29d85f22a01b016ac0f980dfa6a5ca83f726
GimYoungPhil <youngphil.gim@gmail.com>
2018-12-25 00:47:48 +0900

    fixed Footer

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -1,25 +1,25 @@
 <template>
-  <footer class="bg-dark text-white">
+  <footer class="bg-light text-dark">
     <div class="container-fluid p-3 p-md-5">
-        <ul class="nav">
-          <template v-for="(route, index) in routes">
-            <router-link
-              :key="index"
-              class="nav-item"
-              tag="li"
-              :to="{ path: route.path }"
-              active-class="active"
-              exact
-            >
-              <a class="nav-link text-capitalize">
-                {{ route.label }}
-              </a>
-            </router-link>
-          </template>
-        </ul>
-        <p>
-          Designed and built by <a href="https://github.com/GimYoungPhil" target="_blank">@phil</a>.
-        </p>
+      <ul class="nav">
+        <template v-for="(route, index) in routes">
+          <router-link
+            :key="index"
+            class="nav-item"
+            tag="li"
+            :to="{ path: route.path }"
+            active-class="active"
+            exact
+          >
+            <a class="nav-link text-capitalize">
+              {{ route.label }}
+            </a>
+          </router-link>
+        </template>
+      </ul>
+      <p>
+        Designed and built by <a href="https://github.com/GimYoungPhil" target="_blank">@phil</a>.
+      </p>
     </div>
   </footer>
 </template>
@@ -40,5 +40,11 @@ export default {
 <style scoped>
 .nav-item:first-child .nav-link {
   padding-left: 0;
+}
+.nav-link {
+  color: #343a40;
+}
+.nav-link:hover {
+  color: #007bff;
 }
 </style>
