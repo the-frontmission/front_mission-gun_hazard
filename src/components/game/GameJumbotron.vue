166e1e3f9321ff6b03990698196ca448f8ac32c2
GimYoungPhil <youngphil.gim@gmail.com>
2018-12-25 00:48:03 +0900

    add GameJumbotron

--- src/components/game/GameJumbotron.vue
+++ src/components/game/GameJumbotron.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="jumbotron jumbotron-fluid py-0">
+  <div class="jumbotron jumbotron-fluid">
     <div class="container px-0">
-      <img src="./img/front.jpeg" class="img-fluid" alt="">
+      <div class="row">
+        <div class="col-lg-6">
+          <img src="./img/front.jpeg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-6">
+          2064年。<br>
+          天を貫く巨大な塔、世界の平和と協調のシンボルと称された軌道エレベーター「アトラス」は完成した。<br>
+          しかし40年という長期に渡る建造期間は、軌道エレベーター本来の意義を失わせた。<br>
+          皮肉にも同年、平和実現の理想の地とされていたベルゲン共和国で軍事クーデターが勃発する。<br>
+          同国のNORAD軍兵士のアルベルト・グレイブナー（主人公）は、予期せぬクーデターに巻き込まれ、さらに大統領拉致の汚名を着せられ母国を追われる。<br>
+          彼はニューヨークの傭兵派遣機構「カーネルライト」へ所属し、自分を陥れた元上官への復讐、そして不可解なクーデターの陰に隠された真実を知るために戦いに身を投じるのだった･･･
+        </div>
+      </div>
     </div>
   </div>
 </template>
