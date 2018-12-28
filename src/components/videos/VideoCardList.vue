1ecf1eb51040a18977fa8e5c386c2f84d6ce51ac
GimYoungPhil <youngphil.gim@gmail.com>
2018-12-28 22:49:30 +0900

    fixed VideoCardList

--- src/components/videos/VideoCardList.vue
+++ src/components/videos/VideoCardList.vue
@@ -4,19 +4,24 @@
       <div class="col-12">
         <ul class="list-unstyled">
           <template v-for="(video, index) in list">
-            <li class="media mb-2" :key="index">
-              <img class="media-img mr-3" :src="video.imageUrl" alt="image">
-              <div class="media-body text-truncate">
-                <h5 class="text-truncate mt-0 mb-1">{{ video.title }}</h5>
-                {{ video.user }} {{ video.views }} {{ video.published }}
-                <button
-                  class="btn btn-outline-primary btn-sm"
-                  @click="$emit('selectVideo', { uid: video.uid })"
-                >
-                  View
-                </button>
-              </div>
-            </li>
+            <a
+              :key="index"
+              @click.prevent="$emit('selectVideo', { uid: video.uid })"
+              href=""
+              class="text-decoration-none text-reset"
+            >
+              <li class="media mb-2" >
+                <img class="media-img mr-3" :src="video.imageUrl" alt="image">
+                <div class="media-body text-truncate">
+                  <h6 class="media-title text-truncate mt-0 mb-1">
+                    {{ video.title }}
+                  </h6>
+                  <small>
+                    {{ video.user }}
+                  </small>
+                </div>
+              </li>
+            </a>
           </template>
         </ul>
       </div>
@@ -39,6 +44,11 @@ export default {
 <style scoped>
 .media-img {
   height: 5rem;
+}
+.media-title {
+  text-overflow: ellipsis;
+  white-space: normal;
+  -webkit-line-clamp: 2;
 }
 </style>
 
