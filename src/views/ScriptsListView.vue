0a82161205f732e8c062c94c58b8665e20750997
GimYoungPhil <youngphil.gim@gmail.com>
2019-01-01 16:33:40 +0900

    fixde ScriptListView

--- src/views/ScriptsListView.vue
+++ src/views/ScriptsListView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
     <h3 class="display-4 my-4">Scripts</h3>
+    <p>
+      * 본 게임 대사, 번역은
+      <a href="http://culpenetty.egloos.com/" target="_blank">Culpenetty.egloos.com</a>
+      에서 비영리 목적의 사용을 허락 받고 제공된 자료입니다.
+    </p>
     <ScriptsList :scripts="scripts"/>
   </div>
 </template>
